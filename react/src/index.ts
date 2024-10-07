8ad771a3d81c98073214a87f9a027a8f695614ee
Carter Susi <carter4299@gmail.com>
2024-10-07 16:28:39 -0500

    Update index.ts

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,5 +1,5 @@
 export { Put, Get, Del } from './bstore-client';
 export type { BstoreGetResponse, BstoreListResponse, BstoreDelResponse, BstorePutResponse } from './bstore-client';
 
-export { BstoreVideo, BstoreImage, BstoreIFrame, BstoreApplication } from './components';
-export type { BstoreVideoProps, BstoreImageProps, BstoreIFrameProps, BstoreApplicationProps } from './components';
+export { BstoreVideo, BstoreImage, BstoreIFrame, BstoreApplication } from '../components/components';
+export type { BstoreVideoProps, BstoreImageProps, BstoreIFrameProps, BstoreApplicationProps } from '../components/components';
